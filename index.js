976961ec164fd8bb66fc820decdb2bf8a346bc87
Bhuvaneshwaran <bhuvaneshrm@gmail.com>
2023-04-23 19:23:42 +0530

    Hamburger menu added

--- index.js
+++ index.js
@@ -37,3 +37,18 @@ window.onload = function() {
             projects.innerHTML = repos_items;
         })
     }
+
+    var coll = document.getElementsByClassName("nav-toggle");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
