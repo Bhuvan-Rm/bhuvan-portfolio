f29939a7dae780c02339ffd7b0c4d48fbf59ed70
Bhuvaneshwaran <bhuvaneshrm@gmail.com>
2023-05-25 02:19:08 +0530

    updated bg

--- index.js
+++ index.js
@@ -5,64 +5,45 @@ hamburgerMenu.addEventListener('click',()=>{
   hamburgerMenu.classList.toggle('open');
 })
 
-// if ( window.innerWidth < 700 ) {
-//   document.querySelector("div").classList.add('three');
-//   document.querySelector("div").classList.remove('ten');
-// }
 
 let userListItem = [];
 
 
-window.onload = function() {
-    fetch(`https://api.github.com/users/Bhuvan-Rm/repos`)
-        .then((res) => {
-            return res.json()
-        })
-        .then((repos) => {
-            
-            console.log("Repositories found : "+repos.length);
-
-            // count.textContent = "No. of Users found : " + userListItem.length;
-            let repos_items = ""
-            for (let repo of repos) {
-    
-                if(!!repo.homepage){               
-                    repos_items+=`<article class="card zoom">                
-                    <h4 class="repo-name title bold"><b>${repo.name}</b></h4>
-                    <p class="repo-desc desc">${repo.description}</p> 
-                    <a href="${repo.html_url}" target="_blank" class="link-with-icon">Go to repository
-                    <img src="img/icons8-link-48.png" alt="link" style="width:12px;height:12px;" class="link-icon"></a>
-                    <a href="${repo.homepage}" target="_blank" class="link-with-icon">Visit Live Site
-                    <img src="img/icons8-link-48.png" alt="link" style="width:12px;height:12px;" class="link-icon"></a>     
-                    </article> 
-                    `    
-                }
-                else {
-                    repos_items+=`<article class="card zoom">                
-                    <h4 class="repo-name title bold"><b>${repo.name}</b></h4>
-                    <p class="repo-desc desc">${repo.description}</p> 
-                    <a href="${repo.html_url}" target="_blank" class="link-with-icon">Go to repository
-                    <img src="img/icons8-link-48.png" alt="link" style="width:12px;height:12px;" class="link-icon"></a>  
-                    </article> 
-                    ` 
-                }
-                   
-            }
-            projects.innerHTML = repos_items;
-        })
+window.onload = async function() {
+  try {
+    const response = await fetch(`https://api.github.com/users/Bhuvan-Rm/repos`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch repositories');
     }
+    const repos = await response.json();
 
-    var coll = document.getElementsByClassName("nav-toggle");
-    var i;
-    
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-      });
+    console.log("Repositories found: " + repos.length);
+
+    let reposItems = "";
+    for (let repo of repos) {
+      reposItems += `
+        <article class="card zoom">
+          <h4 class="repo-name title bold"><b>${repo.name}</b></h4>
+          <p class="repo-desc desc">${repo.description}</p>
+          <a href="${repo.html_url}" target="_blank" class="link-with-icon">Go to repository
+            <img src="img/icons8-link-48.png" alt="link" style="width:12px;height:12px;" class="link-icon">
+          </a>
+      `;
+      if (!!repo.homepage) {
+        reposItems += `
+          <a href="${repo.homepage}" target="_blank" class="link-with-icon">Visit Live Site
+            <img src="img/icons8-link-48.png" alt="link" style="width:12px;height:12px;" class="link-icon">
+          </a>
+        `;
+      }
+      reposItems += `</article>`;
     }
+    projects.innerHTML = reposItems;
+  } catch (error) {
+    console.error('Error:', error.message);
+    // Handle the error gracefully, e.g., display an error message on the page
+  }
+}
+
+
+
